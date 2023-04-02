2a40be46581d842e9ed2a89a703ae1415dca5620
techchee <ketyung@techchee.com>
2023-04-02 10:56:55 +0800

    update

--- src/contract.rs
+++ src/contract.rs
@@ -4,11 +4,13 @@ use cosmwasm_std::{
     to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult,
 };
 use cw2::set_contract_version;
-
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, HelloResponse, InstantiateMsg, QueryMsg};
+use crate::msg::{ExecuteMsg, QueryMsg};
 use crate::state::{State, STATE};
-
+use crate::ins::{create_sell_offer,update_contract_info};
+use crate::query::get_sell_offers_of;
+use pix0_contract_common::msg::InstantiateMsg;
+use pix0_contract_common::funcs::create_contract_info;
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:pix0-market-contract";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -20,10 +22,13 @@ pub fn instantiate(
     info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    
-    
+        
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     
+    create_contract_info(deps, _env, info.clone() ,_msg.allowed_admins,
+    _msg.treasuries, _msg.fees, _msg.contracts,_msg.log_last_payment)?;
+  
+
     Ok(Response::new()
         .add_attribute("method", "instantiate")
         .add_attribute("owner", info.sender))
@@ -37,18 +42,21 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Dummy {} => try_execute(deps),
-    }
-}
+        
+        ExecuteMsg::UpdateContractInfo { fees, treasuries , contracts,  log_last_payment} =>
+        update_contract_info(deps, _env, info, fees, treasuries, contracts,log_last_payment),
 
-pub fn try_execute(_deps: DepsMut) -> Result<Response, ContractError> {
-    Err(ContractError::Std(StdError::generic_err("Not implemented")))
-    // TODO: Ok(Response::new().add_attribute("method", "try_execute"))
+        ExecuteMsg::CreateSellOffer { offer } => create_sell_offer(deps, _env,_info, offer),
+
+
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::Hello {} => to_binary(&hello_world()?),
+        QueryMsg::GetSellOffersOf {
+            owner, status, start, limit
+        } => to_binary(&get_sell_offers_of(deps, owner, status, start , limit)?),
     }
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,9 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use crate::state::SellOffer;
+use cosmwasm_std::Addr;
+use pix0_contract_common::state::{Fee, Contract};
+
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {}
@@ -8,13 +11,36 @@ pub struct InstantiateMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Dummy {},
+
+    UpdateContractInfo {
+
+        fees : Option<Vec<Fee>>, 
+
+        treasuries : Option<Vec<Addr>>,
+
+        contracts : Option<Vec<Contract>>, 
+
+        log_last_payment : Option<bool>, 
+    },
+
+    CreateSellOffer {
+
+        offer : SellOffer,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    Hello {},
+    GetSellOffersOf {
+        owner : Addr, 
+
+        status : Option<u8>,
+
+        start : Option<u32>,
+
+        limit : Option<32>,
+    },
 }
 
 // We define a custom struct for each query response
